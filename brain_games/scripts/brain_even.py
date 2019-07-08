d05b9536d40962017e6dee1f3164d8e287fd1bdf
natawik <natawik.spb@gmail.com>
2019-07-08 22:44:02 +0300

    add even script

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+from brain_games.scripts.brain_games import main
+from brain_games.even import is_even
+
+
+if __name__ == '__main__':
+    main()
+    print('Answer "yes" if number even otherwise answer "no".')
+    is_even()
