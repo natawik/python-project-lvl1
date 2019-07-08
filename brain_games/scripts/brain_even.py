a74062fa1dc985a018b54cd064bd7c649d0649b0
natawik <natawik.spb@gmail.com>
2019-07-08 23:51:41 +0300

    change main

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 
-from brain_games.scripts.brain_games import main
 from brain_games.even import is_even
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    print('Answer "yes" if number even otherwise answer "no".')
+    is_even()
 
 
 if __name__ == '__main__':
     main()
-    print('Answer "yes" if number even otherwise answer "no".')
-    is_even()
