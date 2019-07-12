73176597dad94f1c15fa261a344ea222d946e5b0
natawik <natawik.spb@gmail.com>
2019-07-13 02:16:29 +0300

    add a calc-script

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+from brain_games.calc import calc
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    print('What is the result of the expression?')
+    calc()
+
+
+if __name__ == '__main__':
+    main()
