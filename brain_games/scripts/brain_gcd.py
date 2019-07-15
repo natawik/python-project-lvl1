225d6ea27bc47926b06f5f2fa4b5478bed4c951e
natawik <natawik.spb@gmail.com>
2019-07-15 23:07:50 +0300

    add gcd-script

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+from brain_games.games.gcd import find_gcd
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    print('Find the greatest common divisor of given numbers.')
+    find_gcd()
+
+
+if __name__ == '__main__':
+    main()
