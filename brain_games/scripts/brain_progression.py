b871ef2f5b5db2b181f56f68af15690566f7955e
natawik <natawik.spb@gmail.com>
2019-08-01 23:56:30 +0300

    add script for game progression

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+from brain_games.games.progression import progr
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    print('What number is missing in the progression?')
+    progr()
+
+
+if __name__ == '__main__':
+    main()
