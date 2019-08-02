112817039dadf781b63fc677a5b525c9ad097025
natawik <natawik.spb@gmail.com>
2019-08-02 17:07:47 +0300

    add script for game is prime

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+from brain_games.games.prime import is_prime
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    is_prime()
+
+
+if __name__ == '__main__':
+    main()
