1283dd0d3d01b9c71b0442924ed1907b589f2662
natawik <natawik.spb@gmail.com>
2019-08-02 15:08:51 +0300

    improve prog

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,30 +1,22 @@
 import prompt
 from brain_games.progr_gen import hidden_number, hide_number, show_progression
-from brain_games.cli import run
+import brain_games.cli
 
 
 def progr():
-    name = run()
-    progression = show_progression()
-    question = "Question: {}"
-    # Shows a progression with missing number
-    wrong_answer = "'{}' is wrong answer ;(. Correct answer was '{}'."
-    try_again = "Let's try again, {}!"
-    # If the user answered incorrectly.
-    correct_answer = "Congratulations, {}!"
-    # If all three answers are correct.
+    prog = show_progression()
     counter = 1
     while(counter <= 3):
-        right_answer = hidden_number(progression)
-        print(question.format(hide_number(progression, right_answer)))
+        right_answer = hidden_number(prog)
+        print(brain_games.cli.question.format(hide_number(prog, right_answer)))
         answer = prompt.string('Your answer: ')
         if (answer == str(right_answer)):
             print('Correct!')
-            progression = show_progression()
+            prog = show_progression()
             counter += 1
         else:
-            print(wrong_answer.format(answer, right_answer))
-            print(try_again.format(name))
+            print(brain_games.cli.wrong_answer.format(answer, right_answer))
+            print(brain_games.cli.try_again.format(brain_games.cli.name))
             break
     else:
-        print(correct_answer.format(name))
+        print(brain_games.cli.correct_answer.format(brain_games.cli.name))
