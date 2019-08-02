d9d4f2357d36b6df78d5b0edd24c1304510aec4c
natawik <natawik.spb@gmail.com>
2019-08-02 15:05:33 +0300

    add common variables for all games

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -6,3 +6,24 @@ def run():
     greeting = "Hello, {}!"
     print(greeting.format(name))
     return(name)
+
+
+name = run()
+
+
+question = "Question: {}"
+# Shows a random number for game "is even?"
+# Shows a random expression for game "calc"
+# Shows a two random numbers for game "great common divisor"
+# Shows a random progression with missing number for game "progression"
+
+
+wrong_answer = "'{}' is wrong answer ;(. Correct answer was '{}'."
+
+
+try_again = "Let's try again, {}!"
+# If the user answered incorrectly.
+
+
+correct_answer = "Congratulations, {}!"
+# If all three answers are correct.
