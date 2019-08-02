2a3fdf627f0da7ea1ecde8f847300a094054af5f
natawik <natawik.spb@gmail.com>
2019-08-02 15:08:17 +0300

    improve even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,31 +1,23 @@
 import prompt
 import brain_games.num_gen
-from brain_games.cli import run
+import brain_games.cli
 
 
 def is_even():
-    name = run()
     number = brain_games.num_gen.show_rand_number()
     # Return a random integer N such that a <= N <= b.
-    question = "Question: {}"
-    # Shows a random number
-    wrong_answer = "'{}' is wrong answer ;(. Correct answer was '{}'."
-    try_again = "Let's try again, {}!"
-    # If the user answered incorrectly.
-    correct_answer = "Congratulations, {}!"
-    # If all three answers are correct.
     counter = 1
     while(counter <= 3):
         right_answer = brain_games.num_gen.is_even_number(number)
-        print(question.format(number))
+        print(brain_games.cli.question.format(number))
         answer = prompt.string('Your answer: ')
         if (answer == right_answer):
             print('Correct!')
             number = brain_games.num_gen.show_rand_number()
             counter += 1
         else:
-            print(wrong_answer.format(answer, right_answer))
-            print(try_again.format(name))
+            print(brain_games.cli.wrong_answer.format(answer, right_answer))
+            print(brain_games.cli.try_again.format(brain_games.cli.name))
             break
     else:
-        print(correct_answer.format(name))
+        print(brain_games.cli.correct_answer.format(brain_games.cli.name))
