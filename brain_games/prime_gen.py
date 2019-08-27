29e5be3f2f2024ea12a571fdbc4fc81674da01ba
natawik <natawik.spb@gmail.com>
2019-08-27 19:25:11 +0300

    arguments as conditions

--- brain_games/prime_gen.py
+++ brain_games/prime_gen.py
@@ -1,16 +1,16 @@
-def is_prime(number):
-    if (number < 2):
+def is_prime(condition):
+    if (condition < 2):
         return True
     div = 2
-    while(div <= number / 2):
-        if (number % div == 0):
+    while(div <= condition / 2):
+        if (condition % div == 0):
             return False
         div += 1
     return True
 
 
-def right_answer(number):
-    if is_prime(number):
+def compare(condition):
+    if is_prime(condition):
         right_answer = 'yes'
     else:
         right_answer = 'no'
