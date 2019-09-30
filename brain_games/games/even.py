35c9c1d08e6b6fb65a5cd0d1ab91a18ae3c971d8
natawik <natawik.spb@gmail.com>
2019-09-30 10:25:53 +0300

    update even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,10 +6,8 @@ DESCRIPTION = 'Answer "yes" if number even otherwise answer "no".'
 
 def make_round():
     number = randint(1, 100)
-    if is_even(number):
-        right_answer = 'yes'
-    else:
-        right_answer = 'no'
+    is_right = is_even(number)
+    right_answer = compare(is_right)
     return number, right_answer
 
 
@@ -17,3 +15,11 @@ def is_even(number):
     if number % 2:
         return False
     return True
+
+
+def compare(is_right):
+    if is_right:
+        right_answer = 'yes'
+    else:
+        right_answer = 'no'
+    return right_answer
